dfa9c803708486f25bc7255724eb88b54915eba4
Xander11rgn <xander11rgn-cool@yandex.ru>
2019-09-27 01:52:35 +0300

    Последнее

--- по 50 видео.py
+++ по 50 видео.py
@@ -2,7 +2,6 @@ import sqlite3
 import datetime
 import re
 from apiclient.discovery import build 
-import seaborn as sns
 import pandas as pd
 import sys
 
@@ -13,6 +12,7 @@ DEVELOPER_KEYS=["AIzaSyDJR3-A7UnPK6ZVPmYPvUfc35iEjb9TqFk",   #Я
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
+#добавить время в имя
 dbname = datetime.datetime.now().strftime("%A_%d-%m-%Y")
 days={'Monday':'Понедельник','Tuesday':'Вторник','Wednesday':'Среда',
       'Thursday':'Четверг','Friday':'Пятница','Saturday':'Суббота','Sunday':'Воскресенье'}
@@ -37,10 +37,13 @@ cursor.execute("""CREATE TABLE result
                FOREIGN KEY (queryID) REFERENCES query (queryID))""")
 conn.commit()
 cursor.execute("""CREATE TABLE video 
-               (likeCount  NOT NULL,
+               (url text NOT NULL,
+               title text NOT NULL,
+               likeCount  NOT NULL,
                dislikeCount  NOT NULL,
                commentCount  NOT NULL,
                viewCount  NOT NULL,
+               date NULL,
                queryID  NOT NULL,
                FOREIGN KEY (queryID) REFERENCES query (queryID))""")
 conn.commit()
@@ -61,7 +64,7 @@ def kowalski():
         queries.append(line)
     for i in range(len(queries)):
         queries[i]=queries[i].replace('\n','')
-    #print(queries)
+    
     
     for i in range(len(queries)):
         query=queries[i]
@@ -95,7 +98,7 @@ def kowalski():
             for j in range(len(DEVELOPER_KEYS)):
                 try:
                     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey = DEVELOPER_KEYS[j]) 
-                    results=youtube.videos().list(part = "statistics",id=videoIds).execute()
+                    results=youtube.videos().list(part = "snippet,statistics",id=videoIds).execute()
                     break
                 except:
                     continue
@@ -104,6 +107,9 @@ def kowalski():
         
             for videoResult in results.get("items", []):
                 l,d,c,v=[0 for f in range(4)]
+                t=videoResult['snippet']['title']
+                date=videoResult['snippet']['publishedAt']
+                url='https://www.youtube.com/watch?v='+videoResult['id']
                 if 'likeCount' in videoResult['statistics']:
                     l=int(videoResult['statistics']['likeCount'])
                     likes=likes+l
@@ -115,7 +121,7 @@ def kowalski():
                     comments=comments+c
                 v=int(videoResult['statistics']['viewCount'])
                 views=views+v
-                cursor.execute("INSERT INTO video VALUES (?,?,?,?,?)",(l,d,c,v,i+1))
+                cursor.execute("INSERT INTO video VALUES (?,?,?,?,?,?,?,?)",(url,t,l,d,c,v,date,i+1))
                 conn.commit()
                 
                 
@@ -145,11 +151,67 @@ def kowalski():
         sys.stdout.write(("\rСбор и анализ данных [ %d"%((i+1)*100/(len(queries)))+"% ] ")+('='*(int((i+1)*10/len(queries)))))
         sys.stdout.flush()
         
-    #print(totalLikes,totalDislikes,totalComments,totalViews)
+    sql="SELECT DISTINCT queryText, totalLikeCount FROM query q JOIN result r ON q.queryID=r.queryID JOIN video v ON r.queryID=v.queryID ORDER BY totalLikeCount DESC"
+    cursor.execute(sql)
+    data=cursor.fetchall()
+    #print(data)
+    query=[]
+    likes=[]
+    for row in data:
+        query.append(row[0])
+        likes.append(row[1])
     
-    #sql="SELECT DISTINCT queryText, totalLikeCount FROM query q JOIN result r ON q.queryID=r.queryID JOIN video v ON r.queryID=v.queryID ORDER BY totalLikeCount DESC"
-    #df=pd.read_sql(sql,conn)
-    #sns.distplot(totalLikes)
+    df=pd.DataFrame()
+    df['Запросы']=query
+    df['Лайки']=likes
+    ax=df.plot(x='Запросы',kind='bar', title='Запросы по убыванию лайков',legend=True,figsize=(len(query), 5),grid=True)
+    ax.set(xlabel="Запросы", ylabel="Лайки")
+    
+    sql="SELECT DISTINCT queryText, totalDislikeCount FROM query q JOIN result r ON q.queryID=r.queryID JOIN video v ON r.queryID=v.queryID ORDER BY totalDislikeCount DESC"
+    cursor.execute(sql)
+    data=cursor.fetchall()
+    query.clear()
+    dislikes=[]
+    for row in data:
+        query.append(row[0])
+        dislikes.append(row[1])
+    
+    df=pd.DataFrame()
+    df['Запросы']=query
+    df['Дизлайки']=dislikes
+    ax=df.plot(x='Запросы',kind='bar', title='Запросы по убыванию дизлайков',legend=True,figsize=(len(query), 5),grid=True)
+    ax.set(xlabel="Запросы", ylabel="Дизлайки")
+    
+    sql="SELECT DISTINCT queryText, totalCommentCount FROM query q JOIN result r ON q.queryID=r.queryID JOIN video v ON r.queryID=v.queryID ORDER BY totalCommentCount DESC"
+    cursor.execute(sql)
+    data=cursor.fetchall()
+    query.clear()
+    comments=[]
+    for row in data:
+        query.append(row[0])
+        comments.append(row[1])
+    
+    df=pd.DataFrame()
+    df['Запросы']=query
+    df['Комментарии']=comments
+    ax=df.plot(x='Запросы',kind='bar', title='Запросы по убыванию комментариев',legend=True,figsize=(len(query), 5),grid=True)
+    ax.set(xlabel="Запросы", ylabel="Комментарии")
+    
+    sql="SELECT DISTINCT queryText, totalViewCount FROM query q JOIN result r ON q.queryID=r.queryID JOIN video v ON r.queryID=v.queryID ORDER BY totalViewCount DESC"
+    cursor.execute(sql)
+    data=cursor.fetchall()
+    #print(data)
+    query.clear()
+    views=[]
+    for row in data:
+        query.append(row[0])
+        views.append(row[1])
+    
+    df=pd.DataFrame()
+    df['Запросы']=query
+    df['Просмотры']=views
+    ax=df.plot(x='Запросы',kind='bar', title='Запросы по убыванию просмотров',legend=True,figsize=(len(query), 5),grid=True)
+    ax.set(xlabel="Запросы", ylabel="Просмотры")
     
     conn.close()
     print('\nЗавершено')
